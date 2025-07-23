20fe8e28cd614f95b008a4ffe307d6e0fb7c7799
David Shalom <david.shalom@sky.uk>
2025-07-23 13:24:33 +0100

    first commit

--- app/src/main/java/com/example/batteryindicator/batteryindicator/Batteryindicator.kt
+++ app/src/main/java/com/example/batteryindicator/batteryindicator/Batteryindicator.kt
@@ -1,7 +1,6 @@
 package com.example.batteryindicator.batteryindicator
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,40 +8,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TabIndicatorScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.batteryindicator.R
 import com.example.batteryindicator.ui.theme.BatteryIndicatorTheme
-import kotlin.concurrent.timer
 
 @Composable
 fun BatteryIndicator(modifier: Modifier = Modifier) {
-
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -57,7 +45,7 @@ fun BatteryIndicator(modifier: Modifier = Modifier) {
             modifier = Modifier.padding(end = 8.dp)
         )
 
-        TabIndicatorScope(modifier = Modifier.weight(1f))
+        Indicator(modifier = Modifier.weight(1f))
 
         Icon(
             painter = painterResource(R.drawable.clover),
@@ -66,15 +54,11 @@ fun BatteryIndicator(modifier: Modifier = Modifier) {
             modifier = Modifier.padding(start = 8.dp)
 
         )
-
-
     }
-
-
 }
 
 @Composable
-fun TabIndicatorScope(modifier: Modifier = Modifier) {
+fun Indicator(modifier: Modifier = Modifier) {
 
     Box(modifier = modifier.fillMaxHeight()) {
 
